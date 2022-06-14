aa10342c42286d6f8aea9629e2e9361b84e01bad
kyudaeshim <skyudae@naver.com>
2022-06-14 23:28:36 +0900

    RIM

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module teamproject {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
+	requires javafx.graphics;
+	requires java.desktop;
+	
+	opens application to javafx.graphics, javafx.fxml, javafx.base; 
+}
